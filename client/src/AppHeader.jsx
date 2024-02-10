67e4242f2f5f8036c72fcc536f7042fc6c613685
Uday <Uda12@uclive.ac.nz>
2024-02-11 00:07:52 +1300

    commit

---
+++ client/src/AppHeader.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function AppHeader(props) {
+  const styles = {
+    fontSize: "8em",
+    color: "grey",
+    justifyContent:"center",
+    alignSelf: "center",
+    display:"flex",
+    margin: 0
+  };
+  //
+  return (
+    <div >
+      <h1 style={styles}>Take Notes</h1>
+    </div>
+  );
+}
+
+export default AppHeader;
